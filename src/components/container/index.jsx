af83163652bc136ed982e7f520e3e41805d8e130
Davi Santos <daviir17@gmail.com>
2023-05-10 17:36:04 -0300

    feat(header): finalização do header e modificação da dimensão do app na tela

---
+++ src/components/container/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import Header from '../header'
+
+export default function Container() {
+  return (
+    <div className='containerTotal'>
+       <div className='containerTotal-content'>
+            <Header/>
+       </div>
+    </div>
+  )
+}
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import image from './Pokeball.png'
+import SearchIcon from '@mui/icons-material/Search';
+import './index.css'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header-container'>
+        
+        <div className='pokedex-content'>
+            <img src={image} alt="Pokeball" />
+            <span>Pokédex</span>
+        </div>
+
+        
+        <form className='form-content' action="#" method="get" autoComplete='of'>
+            <div className='input-content-icon'>
+                <label for='iSearchPokemon'> < SearchIcon className='search-icon'/> </label>
+                <input type='text' name='searchPokemon' id='iSearchPokemon' placeholder='Search'></input>
+            </div>
+
+            <button>
+                #
+            </button>
+    
+        </form>
+
+    </div>
   )
 }
