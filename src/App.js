197a2a10789a72d7aa91ee49d977be1d3373ab13
Davi Santos <daviir17@gmail.com>
2023-05-10 11:17:18 -0300

    feat(estruturação básica): criação das pastasa de header e home do projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/header';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      Header
+      <Header/>
+      <Home/>
     </div>
   );
 }
