58f666fcae3a042a9bcba2f6dfb33cd5f9282575
Davi Santos <daviir17@gmail.com>
2023-05-11 07:42:00 -0300

    feat(botões pokemon): finalização da implementação base dos botões de cada pokemon e componetização

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './index.css';
+import PokemonButton from '../../components/pokemonButton';
 
 export default function Home() {
   return (
     <div className='home-container'>
       <div className='home-content'>
-        home
+          
+        <PokemonButton/>
+        <PokemonButton/>
+        <PokemonButton/>
+        <PokemonButton/>
+        <PokemonButton/>
+        <PokemonButton/>
+        <PokemonButton/>
+        <PokemonButton/>
+        <PokemonButton/> 
+          
       </div>
     </div>
   )
