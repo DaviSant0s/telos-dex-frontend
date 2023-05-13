d71f845727585b3fdfc8d889fab453df0a76c9de
Davi Santos <daviir17@gmail.com>
2023-05-12 22:30:02 -0300

    feat(measureabout): finalização da implementação da seção about da página de detalhes dos pokemons

--- src/components/pokemonsDetails/index.jsx
+++ src/components/pokemonsDetails/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './index.css';
 import Pokeball from './Pokeball.png';
 import pokemon1 from './pokemon1.png';
+import icon1 from './icon1.png'
+import icon2 from './icon2.png'
 import TypePokemon2 from '../typePokemon2';
 
 
@@ -12,7 +14,34 @@ export default function PokemonsDetails() {
         <img className='pokemon1-detail' src={pokemon1} alt="pokemon1" />
         <div className='details-content'>
             <div className='type-pokemon'>
-              <TypePokemon2 type1={'Grass'} type2={'Poison'} color1={'#74CB48'} color2={'#A43E9E'} width1={'46px'} width2={'51px'}/>
+              <TypePokemon2 
+              type1={'Grass'} 
+              type2={'Poison'} 
+              color1={'#74CB48'} 
+              color2={'#A43E9E'} 
+              width1={'46px'} 
+              width2={'51px'}
+              />
+
+              <div className='aboutPokemon'>About</div>
+
+              <div className='especificationsPokemon'>
+                <div className='weight especificatios'>
+                  <div className='measure-weight measure'><img src={icon1} alt="iconWeight" />6,9 kg</div>
+                  <span>Weight</span>
+                </div>
+
+                <div className='height especificatios'>
+                  <div className='measure-height measure'><img src={icon2} alt="iconHeight" />0,7 m</div>
+                  <span>Height</span>
+                </div>
+
+                <div className='moves especificatios'>
+                  <div className='measure-moves'><span>Chlorophyll</span><span>Overgrow</span></div>
+                  <span>Moves</span>
+                </div>
+              </div>
+
             </div>
         </div>
     </div>
